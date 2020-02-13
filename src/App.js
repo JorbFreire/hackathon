a7b046bd30c1959e0b2975c82977d9554ec506a3
JorbFreire <jorbfreire@ufrn.edu.br>
2020-02-13 19:17:57 -0300

    empty skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import LoginPage from './pages/LoginPage';
+
 function App() {
   return (
     <div className="App">
-      <h1> Hello </h1>
+      <LoginPage />
     </div>
   );
 }
---
+++ src/pages/LoginPage/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styles.css';
+
+const LoginPage = () => (
+  <div className="page" id="LoginPage">
+    <header>
+      
+    </header>
+    <main>
+      <div id="LoginCollum">
+        
+      </div>
+      <div id="RegistrationArtCollum">
+        
+      </div>
+    </main>
+  </div>
+);
+
+export default LoginPage;
