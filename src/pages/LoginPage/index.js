1409fc1799371137a63f5da1fe08c84e616a3d66
JorbFreire <jorbfreire@ufrn.edu.br>
2020-02-13 22:49:06 -0300

    login html

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -8,10 +8,23 @@ const LoginPage = () => (
     </header>
     <main>
       <div id="LoginCollum">
-        
+        <h2> Entre na Sua Conta </h2>
+        <form>
+          <label htmlFor="UserEmail">Email </label>
+          <input type="text" id="UserEmail"/>
+
+          <label htmlFor="UserPassword">Password </label>
+          <input type="text" id="UserPassword"/>
+
+          <a href=""> Esqueceu sua senha? </a>
+          <button type="submit"> Entrar </button>
+        </form>
+        <button type="submit"> Logar com o Google </button>
       </div>
       <div id="RegistrationArtCollum">
-        
+        <h2>Ainda não possui uma conta? </h2>
+        <button>Cadastre-se </button>
+        <img src="" alt=""/>
       </div>
     </main>
   </div>
