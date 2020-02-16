9bdd827cad1c55e2be435a8bf994fa29471864ac
JorbFreire <jorbfreire@ufrn.edu.br>
2020-02-16 17:27:04 -0300

    initi display page

---
+++ src/components/AppMemberMenu/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css'
+
+const AppMemberMenu = () => (
+  <>
+    <menu>
+      <p>Display</p>
+      <p>Treinamento</p>
+      <p>Perguntas</p>
+    </menu>
+  </>
+);
+
+export default AppMemberMenu;
--- src/pages/Display/index.js
+++ src/pages/Display/index.js
@@ -1,8 +1,53 @@
-import React from 'react';
-import './styles.css'
+import React, { useState, useEffect } from 'react';
 
-const Display = () => (
-  <h2>Display</h2>
-);
+import HeaderBar from '../../components/HeaderBar';
+import AppMemberMenu from '../../components/AppMemberMenu';
+import './styles.css';
+
+const Display = () => {
+  const [workersList, setWorkersList] = useState([{}]);
+
+  function findWorkersForThisCompany() {
+    setWorkersList([{
+      name: "this is my name1",
+      learning: "this is how much I learned",
+      productivity: "this is how much I work",
+      acuaity: "this is how much time I speend on work",
+    },
+    {
+      name: "this is my name2",
+      learning: "this is how much I learned",
+      productivity: "this is how much I work",
+      acuaity: "this is how much time I speend on work",
+    },
+    {
+      name: "this is my name3",
+      learning: "this is how much I learned",
+      productivity: "this is how much I work",
+      acuaity: "this is how much time I speend on work",
+    },]);
+    console.log(workersList);
+
+  }
+
+  useEffect( () => {
+    findWorkersForThisCompany();
+  }, []);
+
+  return (
+    <>
+      <HeaderBar />
+      <AppMemberMenu />
+      <button onClick={() => {
+        console.log(workersList);
+      }}> debug api</button>
+      {workersList.forEach(element => (
+        <div className="user">
+          <p>{element.name}</p>
+        </div>
+      ))}
+    </>
+  );
+} 
 
 export default Display;
